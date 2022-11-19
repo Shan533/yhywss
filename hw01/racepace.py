bfe587c2d11be7c4e4e469113a5afc2ec64b9b42
shanshan533 <wu.shans@northeastern.edu>
2022-11-18 17:54:01 -0800

    correct hw

--- hw01/racepace.py
+++ hw01/racepace.py
@@ -1,13 +1,26 @@
-distance = float(input("How many kilometers did you run? \n"))
-hours = int(input("What was your finish time? Enter hours: \n"))
-minutes = int(input("Enter minutes: \n"))
+'''
+CS5001
+Haiyang Yu
+compute speed
+'''
 
-distance_miles = distance / 1.61
-time_total_minutes = 60 * hours + minutes
-time_total_hours = hours + minutes / 60
 
-pace_in_mins = time_total_minutes / distance_miles
-pace_in_hours = time_total_hours / distance_miles
+def main():
+    distance = float(input("How many kilometers did you run? \n"))
+    hours = int(input("What was your finish time? Enter hours: \n"))
+    minutes = int(input("Enter minutes: \n"))
 
-print(str(round(distance_miles, 2)) + " miles\n", str(round(pace_in_mins, 2)) + " pace\n",
-str(round(pace_in_hours, 2)) + " MPH\n")
+    distance_miles = distance / 1.61
+    time_total_minutes = 60 * hours + minutes
+    time_total_hours = hours + minutes / 60
+
+    pace_in_mins = time_total_minutes / distance_miles
+    pace_in_hours = time_total_hours / distance_miles
+
+    print(str(round(distance_miles, 2)) + " miles\n", str(round(pace_in_mins, 2)) + " pace\n",
+    str(round(pace_in_hours, 2)) + " MPH\n")
+    print("%s:%s" % (time_total_hours, time_total_minutes))
+
+
+if __name__ == "__main__":
+    main()
