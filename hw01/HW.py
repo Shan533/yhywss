b7170f0f5c2ce43bbc5fb8bd1aee282a72feda1b
Haiyang <haiyang@MacBook-Pro-2.local>
2022-11-20 19:19:34 -0800

    fixed several styling issues

--- hw01/HW.py
+++
@@ -1,4 +0,0 @@
-# Q1
-    # int ； in ； string ; boolean ; float ; string ; int ; float
-#Q2
-    # 
--- hw02/Lab2/problem_1.py
+++ hw02/Lab2/problem_1.py
@@ -1,5 +1,5 @@
 def main():
-    
+
     '''
     1.input the amount of total money
     2. tips from 0-1
@@ -8,7 +8,8 @@ def main():
     '''
 
     amount = float(input("how much was this bill? \n"))
-    tips = float(input("how much tips were this bill with? \nPlease input a number from 0 to 1 \n"))
+    tips = float(input("how much tips were this bill with? \nPlease input a \
+                       number from 0 to 1 \n"))
     persons = float(input("how many people were this bill with? \n"))
 
     total_bill = amount + tips
